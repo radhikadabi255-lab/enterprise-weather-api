6e3f1a4337a5d23fb813c3f3d426d01bcb67dd1a
Radhika Dabi <your_radhikadabi255@gmail.com>
2026-06-22 20:53:43 +0530

    Fixed Flask host for Docker

--- app.py
+++ app.py
@@ -50,5 +50,5 @@ def weather():
         "temperature": data["current"]["temperature_2m"]
     })
 if __name__ == "__main__":
-    app.run(debug=True)
+   app.run(host="0.0.0.0", port=5000, debug=True)
     
