ea082b994fab00e937391438529ba0c33f47d75d
Ubuntu <ubuntu@ip-172-31-30-113.ec2.internal>
2026-06-28 19:18:34 +0000

    Added humidity and wind details

--- app.py
+++ app.py
@@ -8,16 +8,16 @@ def home():
     return jsonify({
         "project": "Enterprise Weather API",
         "status": "Running Successfully",
-        "developer": "Radhika"
+        "developer": "Radhika",
+        "version": "2.0"
     })
+
 
 @app.route("/weather")
 def weather():
 
-    # User se city lena
     city = request.args.get("city", "Pune")
 
-    # Supported cities
     cities = {
         "Pune": (18.52, 73.85),
         "Delhi": (28.61, 77.20),
@@ -25,30 +25,46 @@ def weather():
         "Bangalore": (12.97, 77.59)
     }
 
-    # Check city
     if city not in cities:
         return jsonify({
-            "error": "City not supported"
+            "error": "City not supported",
+            "supported_cities": list(cities.keys())
         }), 404
 
-    # Latitude & Longitude
     latitude, longitude = cities[city]
 
-    # Weather API URL
-    url = f"https://api.open-meteo.com/v1/forecast?latitude={latitude}&longitude={longitude}&current=temperature_2m"
+    url = (
+        f"https://api.open-meteo.com/v1/forecast?"
+        f"latitude={latitude}"
+        f"&longitude={longitude}"
+        f"&current=temperature_2m,relative_humidity_2m,wind_speed_10m,wind_direction_10m"
+    )
+
     try:
         response = requests.get(url)
+        response.raise_for_status()
         data = response.json()
 
-    except Exception as e:
+        current = data["current"]
+
+    except Exception:
         return jsonify({
             "error": "Unable to fetch weather data"
         }), 500
 
     return jsonify({
+        "developer": "Radhika",
+        "project": "Enterprise Weather API",
         "city": city,
-        "temperature": data["current"]["temperature_2m"]
+        "latitude": latitude,
+        "longitude": longitude,
+        "temperature": current["temperature_2m"],
+        "humidity": current["relative_humidity_2m"],
+        "wind_speed": current["wind_speed_10m"],
+        "wind_direction": current["wind_direction_10m"],
+        "time": current["time"]
     })
+
+
 if __name__ == "__main__":
-   app.run(host="0.0.0.0", port=5000, debug=True)
-    
+    app.run(host="0.0.0.0", port=5000, debug=True)
